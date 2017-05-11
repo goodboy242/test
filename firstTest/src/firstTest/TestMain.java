4e2bc6675ef3e63268f9935aba3a6a9c939944a2
goodboy242 <goodboy242@daum.net>
2017-05-12 01:15:20 +0900

    added in hp

--- firstTest/src/firstTest/TestMain.java
+++ firstTest/src/firstTest/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		
 		System.out.println("my git test will be completed in r32 room");
-		
+		System.out.println("added in hp");
 		
 	}
 
